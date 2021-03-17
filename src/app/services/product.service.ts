3262cf4e6b0f1a9b8a260c81d666c42a8f227021
ozturkk <50148360+ozturkk@users.noreply.github.com>
2021-03-17 22:59:55 +0300

    13 Mart

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';//Backend e istekte bulunmak için
-import { ProductResponseModel } from '../models/productResponseModel';
+
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/ListResponseModel';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  apiUrl='https://localhost:44315/api/products/getall';
+  apiUrl='https://localhost:44315/api/';
   constructor(private httpClient:HttpClient) { }
-  getProducts():Observable<ProductResponseModel>{
+  getProducts():Observable<ListResponseModel<Product>>{
+    let newPath=this.apiUrl+"products/getall"
     return this.httpClient
-    .get<ProductResponseModel>(this.apiUrl)
+    .get<ListResponseModel<Product>>(newPath)
     //gelen datayı ProductResponseModel e map eder
+  }
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
+    let newPath=this.apiUrl+"products/getbycategory?categoryId="+categoryId
+    return this.httpClient
+    .get<ListResponseModel<Product>>(newPath)
+    
   }
 }
